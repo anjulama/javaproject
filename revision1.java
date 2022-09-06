2e3e7ec6eca9b63ce796780ee98b50b70911491c
anjulama <anjulama1402@gmail.com>
2022-09-06 17:37:56 -0500

    rev1

---
+++ revision1.java
@@ -0,0 +1,30 @@
+package first_project_1;
+
+public class revision1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	//	toUpperCase()
+		String name = "anju lama";
+		String upperCasename = name.toUpperCase();
+		System.out.println(upperCasename);
+		
+    // toLowerCase()
+		String country = "NEPAL";
+		String lowerCasecountry = country.toLowerCase();
+		System.out.println(lowerCasecountry);
+				
+    // length()
+		String city = "kathmandu";
+		int a = city.length();
+		System.out.println(a);
+		
+	//	or 
+		 System.out.println(city.length());
+		 
+		 
+	}
+		
+}
